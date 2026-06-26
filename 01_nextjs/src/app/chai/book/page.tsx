3cfb2dd683e3366552b4090788232446596d5032
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-26 22:22:03 +0530

    Structuring the next.js project.

--- 01_nextjs/src/app/chai/book/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function BookPage() {
-  return <h1 className="text-black text-5xl text-center">Book Page </h1>;
-}
--- 01_nextjs/src/app/chai/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Chai() {
-  return <h1 className="text-4xl text-black-700 text-center mt-5">Hello Chai page. </h1>;
-}
