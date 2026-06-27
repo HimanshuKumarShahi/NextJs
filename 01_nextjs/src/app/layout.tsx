8b7b9c6729e161ecddabf9333ae5919a19c745bb
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-27 20:42:35 +0530

    Redesign and debug errors.

--- 01_nextjs/src/app/layout.tsx
+++ 01_nextjs/src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +29,13 @@ export default function RootLayout({
   return (
     <>
       <html
-        lang="en"
-        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+        lang="en" 
+        className={cn(",", "dark:bg-black", "dark:text-white", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
       >
         <body className="min-h-full flex flex-col">
-          <Navbar />
+          <div className="relative w-full flex items-center justify-center">
+            <Navbar />
+          </div>
           {children}
         </body>
       </html>
