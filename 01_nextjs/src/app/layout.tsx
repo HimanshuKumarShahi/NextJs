fd26bad4c0d8825bf82ddcdbbba10fb099308c7d
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-27 18:41:01 +0530

    attach navbar in the top of layout page.

--- 01_nextjs/src/app/layout.tsx
+++ 01_nextjs/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,14 +25,15 @@ export default function RootLayout({
 }>) {
   return (
     <>
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      <html
+        lang="en"
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       >
-      <body className="min-h-full flex flex-col">
-        {children}
+        <body className="min-h-full flex flex-col">
+          <Navbar />
+          {children}
         </body>
-    </html>
-      </>
+      </html>
+    </>
   );
 }
