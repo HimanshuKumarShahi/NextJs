2665879068d163ad69c9dbef836f6f77fdf41263
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-29 17:21:12 +0530

    designing navbar page

--- 01_nextjs/src/components/Navbar.tsx
+++ 01_nextjs/src/components/Navbar.tsx
@@ -1,32 +1,20 @@
 "use client";
-import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, } from "./ui/navbar-menu";
+
+import { useState } from "react";
+import { HoveredLink, Menu, MenuItem } from "./ui/navbar-menu";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
-export function NavbarDemo() {
-  return (
-    <div className="w-full flex items-center justify-center">
-      <Navbar className="top-2" />
-    </div>
-  );
-}
-
 function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
+
   return (
     <div
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
         <Link href={"/"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Home"
-          >
-
-          </MenuItem>
+          <MenuItem setActive={setActive} active={active} item="Home" />
         </Link>
         <MenuItem
             setActive={setActive}
@@ -60,4 +48,5 @@ function Navbar({ className }: { className?: string }) {
     </div>
   );
 }
+
 export default Navbar;
