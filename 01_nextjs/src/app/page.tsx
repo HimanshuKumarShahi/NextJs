86cd6127dbc6a493a81f62d48dd678045fe9c4c0
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-29 16:07:49 +0530

    design home page.

--- 01_nextjs/src/app/page.tsx
+++ 01_nextjs/src/app/page.tsx
@@ -1,12 +1,9 @@
-import HeroSection from '@/components/HeroScetion'
+import HeroSection from "@/components/HeroScetion";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white antialiased bg-grid-black">
-      <h1 className="text-5xl text-center ">Test Next.js</h1>
-
-    <HeroSection />
-
+      <HeroSection />
     </main>
   );
 }
