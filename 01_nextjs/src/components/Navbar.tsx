745ba2036d605a9e2579a0f56fe99114dfa680e7
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-29 16:50:23 +0530

    design the navbar

--- 01_nextjs/src/components/Navbar.tsx
+++ 01_nextjs/src/components/Navbar.tsx
@@ -1,57 +1,44 @@
 "use client";
-import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+
+import { useState } from "react";
+import { HoveredLink, Menu, MenuItem } from "./ui/navbar-menu";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
-export function NavbarDemo() {
-  return (
-    <div className="w-full flex items-center justify-center">
-      <Navbar className="top-2" />
-    </div>
-  );
-}
-
 function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
+
   return (
     <div
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
         <Link href={"/"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Home"
-          >
-
-          </MenuItem>
+          <MenuItem setActive={setActive} active={active} item="Home" />
         </Link>
-        <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Courses"
-          >
-            <div className="flex flex-col space-y-4 text-">
-              <HoveredLink href="/courses">All Courses </HoveredLink>
-              <HoveredLink href="/courses">Web Development </HoveredLink>
-              <HoveredLink href="/courses">Advance Composition </HoveredLink>
-              <HoveredLink href="/courses"> Blog Writing </HoveredLink>
-              <HoveredLink href="/courses">Production </HoveredLink>
-            </div>
-          </MenuItem>
-           <Link href={"/"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Contact Us"
-          >
-            
-          </MenuItem>
+
+        <MenuItem setActive={setActive} active={active} item="Our Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses/web-development">
+              Web Development
+            </HoveredLink>
+            <HoveredLink href="/courses/ai-agents">AI & Agents</HoveredLink>
+            <HoveredLink href="/courses/backend">
+              Backend Masterclass
+            </HoveredLink>
+            <HoveredLink href="/courses/production">
+              Production Ready
+            </HoveredLink>
+          </div>
+        </MenuItem>
+
+        <Link href={"/contact"}>
+          <MenuItem setActive={setActive} active={active} item="Contact Us" />
         </Link>
       </Menu>
     </div>
   );
 }
+
 export default Navbar;
