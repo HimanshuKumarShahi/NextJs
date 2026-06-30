04c03ab1892cc834e92b55ca6d82d37982cc839c
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-30 22:49:50 +0530

    configuring home page

--- 01_nextjs/src/app/page.tsx
+++ 01_nextjs/src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/HeroScetion";
+import Courses from "@/components/Courses"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <Courses />
     </main>
   );
 }
