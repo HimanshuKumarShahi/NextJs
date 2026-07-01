8cc7c861c37b113eb22311105f2b1d0caaa5da1d
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-07-01 15:40:33 +0530

    adding contact routes of courses page

---
+++ 01_nextjs/src/app/courses/page.jsx
@@ -0,0 +1,9 @@
+import Courses from "@/components/Courses";
+
+export default function CoursesRoute() {
+  return (
+    <main className="min-h-screen bg-black pt-20">
+      <Courses />
+    </main>
+  );
+}
