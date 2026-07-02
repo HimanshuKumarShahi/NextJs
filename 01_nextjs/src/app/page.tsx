bd01fcf506467226f0efb258087cc7853d59bc49
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-07-02 11:52:06 +0530

    Adding contact page in layout

--- 01_nextjs/src/app/page.tsx
+++ 01_nextjs/src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroScetion";
 import Courses from "@/components/Courses"
+import ContactPage from "@/components/Contact";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <Courses />
+      <ContactPage />
     </main>
   );
 }
