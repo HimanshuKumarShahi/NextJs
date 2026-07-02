8cb498022191e841ccf48f5d1b00979cf7efedd5
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-07-02 17:45:20 +0530

    adding to home page .

--- 01_nextjs/src/app/page.tsx
+++ 01_nextjs/src/app/page.tsx
@@ -1,12 +1,15 @@
 import HeroSection from "@/components/HeroScetion";
-import Courses from "@/components/Courses"
+import Courses from "@/components/Courses";
 import ContactPage from "@/components/Contact";
+import StickyRoller from  "@/components/Sticky"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      
       <HeroSection />
       <Courses />
+      <StickyRoller />
       <ContactPage />
     </main>
   );
