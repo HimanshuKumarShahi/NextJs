d1e95a0189db0af2df294780f7c683205066876c
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-07-10 16:33:45 +0530

    Schema for User

--- messager/src/model/User.ts
+++ messager/src/model/User.ts
@@ -17,3 +17,52 @@ const MessageSchema: Schema<Message> = new Schema({
   },
 });
 
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpires: Date;
+  isVerified: boolean;
+  isAcceptingMessages: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Please provide valid email address👍🏼.",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required."],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify Code is required."],
+  },
+  verifyCodeExpires: {
+    type: Date,
+    required: [true, "Verify code Expiry is required."],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessages: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
