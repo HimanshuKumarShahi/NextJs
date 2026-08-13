5bcfc0de4f0c1f2407533dbf7b47a5c0d4e83352
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-08-13 21:05:49 +0530

    create new routes name project with button

---
+++ 02_nextjs/src/app/project/page.tsx
@@ -0,0 +1,11 @@
+import Button from "@/components/button";
+
+export default function project() {
+  return (
+    <div className="text-white text-sm p-4 flex-row bg-auto">
+        <h1>Project page </h1>
+        <p>Project page is working</p>
+        <Button />
+    </div>
+  )
+}
