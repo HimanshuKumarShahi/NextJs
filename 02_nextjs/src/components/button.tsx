62cb0c71766119c9e1e21320837bf8d0a7cf2f94
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-08-13 21:05:25 +0530

    design client side button design

---
+++ 02_nextjs/src/components/button.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+export default function Button() {
+  return (
+    <div>
+         <button onClick={()=>{
+            alert("Button Clicked 😎")
+        }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
+            Click Me !
+        </button>
+    </div>
+  )
+}
