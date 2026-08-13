fe62f5c0ca10f1c5cf7b2b4d987e1f8dd5891269
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-08-13 21:06:43 +0530

    configure all in layout page.

--- 02_nextjs/src/app/layout.tsx
+++ 02_nextjs/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,18 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <nav className="bg-gray-400 text-white p-4 flex gap-5 ">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/project">Project's</Link>
+          <Link href="/project/web_devlopment">web_development</Link>
+          <Link href="/project/python">Python</Link>
+        </nav>
+        <div className="text-white text-sm p-4 flex-row">
+        {children}
+        </div>
+      </body>
     </html>
   );
 }
