5a76545cef4547b32834bd49e9bb19d962fe9686
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-08-14 20:36:00 +0530

    adding loading in next js for fetching data

---
+++ 02_nextjs/src/app/project/python/loading.tsx
@@ -0,0 +1,8 @@
+
+export default function Loading(){
+    return(
+        <main className="flex flex-col items-center justify-center min-h-screen py-2">
+          <h1 className="text-2xl font-bold">Loading...</h1>
+        </main>
+    )
+}
