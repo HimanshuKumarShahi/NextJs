163b5dde48824f347d61682763edbb64a79faa27
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-08-14 20:24:42 +0530

    implement filter user

--- 02_nextjs/src/app/project/python/page.tsx
+++ 02_nextjs/src/app/project/python/page.tsx
@@ -1,27 +1,17 @@
-type User={
-  id:number;
-  name:string;
-  username:string;
-}
+import UsersFilter from "@/components/usersfilter";
 
-export default async function PythonPage(){
-
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+export default async function PythonPage() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
 
   const users = await response.json();
 
-  return(
+  return (
     <>
-    <main>
-      <h1>Users Data: </h1>
-      <p className="p-5 bg-orange-100 text-black w-auto">User Data for trial</p>
-    
-      <ul>
-        {users.map((user:User)=>(
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    </main>
+      <main className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1>Person Search:</h1>
+        <p>Search for a person by name:</p>
+        <UsersFilter users={users} />
+      </main>
     </>
-  )
+  );
 }
